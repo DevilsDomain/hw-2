a1a30348254bbd6c92f42a3f6a562f53c5efee2b
DevilsDomain <alaraerzincan@gmail.com>
2023-03-15 16:37:33 +0100

    first commit

---
+++ src/app/head.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Head() {
+  return (
+    <>
+    <title>Home Page</title>
+    <meta name="home page" content="index page" />
+    </>
+  )
+}
+
+export default Head

1d9e2caf6cc7de30c3cb9252b052a3c81a23b148
DevilsDomain <alaraerzincan@gmail.com>
2023-03-16 11:56:42 +0100

    intalled tailwind, made image responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
-
-
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import image from '../../public/thumb-1920-69561 copy.jpeg'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,10 +12,18 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-        <main>
-          <p>Home</p>
-        </main>
-    
+      <main>
+        <p>Home Page</p>
+        <div className="relative h-0" style={{ paddingBottom: "56.25%" }}>
+          <Image
+            src={image}
+            alt="Picture of the author"
+            className="absolute object-cover w-full h-full"
+            width={1000}
+            height={1000}
+          />
+        </div>
+      </main>
     </>
   )
 }
